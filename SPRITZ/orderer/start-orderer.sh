b89c0cc00d64f7ce1441b3922abf4846d30a9e7e
Prajjwal <pujjumehta@gmail.com>
2020-08-13 20:41:32 +0530

    demo 1/2 steps done

--- SPRITZ/orderer/start-orderer.sh
+++
@@ -1,5 +0,0 @@
-echo "Make sure you've set all the identities beforehand."
-./generate-genesis.sh
-./generate-channel-tx.sh
-echo "launching orderer binary in solo ordering mode."
-orderer
---
+++ backend-cc/tokenB.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/darksidergod/githubfs-test"
+	"github.com/google/go-github/github"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-protos-go/peer"
+	"golang.org/x/oauth2"
+)
+
+type mycc struct{}
+
+var m map[string]string
+
+// Init Implements the Init method
+func (token *mycc) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	fmt.Println("Init executed")
+	return shim.Success([]byte("true"))
+}
+
+func (clientdid *mycc) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+
+	funcName, args := stub.GetFunctionAndParameters()
+	fmt.Println("Function=", funcName)
+
+	if funcName == "read" {
+		return clientdid.read(stub, args[0], args[1])
+
+	} else if funcName == "write" {
+		return clientdid.write(stub, args[0], args[1])
+	}
+
+	return shim.Error(("Bad Function Name = " + funcName + "!!!"))
+}
+
+func (clientdid *mycc) read(stub shim.ChaincodeStubInterface, patientID string, levelToRead string) peer.Response {
+	githubToken := "5a55cf8ede14add10195364f3daf62e8e9944ba5"
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	fs, err := githubfs.NewGithubfs(client, "darksidergod", "recordStorage", "master")
+	if err != nil {
+		panic(err)
+	}
+	path := m[patientID]
+	path += levelToRead
+	f, err := fs.Open(path)
+	if err != nil {
+		return shim.Error("failure")
+	}
+	fmt.Println(f)
+	return shim.Success([]byte("Success."))
+}
+
+func (clientdid *mycc) write(stub shim.ChaincodeStubInterface, patientID string, levelToRead string) peer.Response {
+	githubToken := "5a55cf8ede14add10195364f3daf62e8e9944ba5"
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	fs, err := githubfs.NewGithubfs(client, "darksidergod", "recordStorage", "master")
+	if err != nil {
+		panic(err)
+	}
+	f, err := fs.OpenFile("dark", os.O_APPEND, 0644)
+	f.Write([]byte("Hello World."))
+	err = f.Close()
+	if err != nil {
+		return shim.Error("error")
+	}
+
+	return shim.Success([]byte("Success."))
+}
+
+// Chaincode registers with the Shim on startup
+func main() {
+	fmt.Printf("Started Chaincode.\n")
+	err := shim.Start(new(mycc))
+	if err != nil {
+		fmt.Printf("Error starting chaincode: %s", err)
+	}
+}
