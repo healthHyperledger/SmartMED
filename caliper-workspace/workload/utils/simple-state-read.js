a68df4e4cce2316b981b156192efbcd4ed4637e8
darksider <pujjumehta@gmail.com>
2020-12-10 01:45:58 +0530

    added caliper workspace

---
+++ caliper-workspace/workload/utils/simple-state-read.js
@@ -0,0 +1,104 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+const Dictionary = 'abcdefghijklmnopqrstuvwxyz';
+
+/**
+ * Class for managing simple account states.
+ */
+class SimpleState {
+
+    /**
+     * Initializes the instance.
+     */
+    constructor(dataOwner, levelRequested=1) {
+        this.dataOwner = dataOwner;
+        this.levelRequested = levelRequested;       
+    }
+
+    /**
+     * Generate string by picking characters from the dictionary variable.
+     * @param {number} number Character to select.
+     * @returns {string} string Generated string based on the input number.
+     * @private
+     */
+    _get26Num(number){
+        let result = '';
+
+        while(number > 0) {
+            result += Dictionary.charAt(number % Dictionary.length);
+            number = parseInt(number / Dictionary.length);
+        }
+
+        return result;
+    }
+
+    /**
+     * Construct an account key from its index.
+     * @param {number} index The account index.
+     * @return {string} The account key.
+     * @private
+     */
+    _getAccountKey(index) {
+        return this.accountPrefix + this._get26Num(index);
+    }
+
+    /**
+     * Returns a random account key.
+     * @return {string} Account key.
+     * @private
+     */
+    _getRandomAccount() {
+        // choose a random TX/account index based on the existing range, and restore the account name from the fragments
+        const index = Math.ceil(Math.random() * this.accountsGenerated);
+        return this._getAccountKey(index);
+    }
+
+    /**
+     * Get the arguments for creating a new account.
+     * @returns {object} The account arguments.
+     */
+    getOpenAccountArguments() {
+        return {
+            account: this.dataOwner,
+            money: this.levelRequested
+        };
+    }
+
+    /**
+     * Get the arguments for querying an account.
+     * @returns {object} The account arguments.
+     */
+    getQueryArguments() {
+        return {
+            account: this._getRandomAccount()
+        };
+    }
+
+    /**
+     * Get the arguments for transfering money between accounts.
+     * @returns {object} The account arguments.
+     */
+    getTransferArguments() {
+        return {
+            source: this._getRandomAccount(),
+            target: this._getRandomAccount(),
+            amount: this.moneyToTransfer
+        };
+    }
+}
+
+module.exports = SimpleState;
